654a7b5896bdc5821f1bc40cf7b5bb4be8978dac
sanchxt <92094343+sanchxt@users.noreply.github.com>
2023-12-10 02:16:34 +0530

    Add files via upload

---
+++ screens/drawers/ContactsDrawer.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function ContactsDrawer() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Contacts</Text>
+    </View>
+  );
+}
---
+++ screens/drawers/RateAppDrawer.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function RateAppDrawer() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Rate App</Text>
+    </View>
+  );
+}
