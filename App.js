1468e53711d80cd922733e4b798d27cc66739331
sanchxt <92094343+sanchxt@users.noreply.github.com>
2023-12-10 02:13:10 +0530

    Delete App.js

--- App.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import StackNavigator from "./StackNavigator";
-// import { AuthProvider } from "./hooks/useAuth";
-
-export default function App() {
-  return <StackNavigator />;
-}
