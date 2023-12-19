bf0df7b7521224e410ff4c63242b38cbb569ec7c
sanchxt <92094343+sanchxt@users.noreply.github.com>
2023-12-19 15:42:43 +0530

    Add files via upload

---
+++ screens/drawers/SubmitCaseDrawer.js
@@ -0,0 +1,150 @@
+import React, { useState, useContext } from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import axios from "axios";
+import { apiBaseUrl } from "../../apiConfig"; 
+import { UserContext } from "../../UserContext";
+import * as SecureStore from "expo-secure-store";
+
+import KeyboardAvoidingWrapper from "../../components/KeyboardAvoidingWrapper";
+
+// for form
+import { Formik } from "formik";
+
+// for icons
+import { Octicons, Ionicons, Fontisto } from "@expo/vector-icons";
+
+import {
+  StyledContainer,
+  InnerContainer,
+  PageLogo,
+  PageTitle,
+  SubTitle,
+  StyledFormArea,
+  LeftIcon,
+  RightIcon,
+  StyledInputLabel,
+  StyledTextInput,
+  BreakPaddingTop,
+  StyledButton,
+  ButtonText,
+  Colors,
+  MsgBox,
+  Line,
+  ExtraView,
+  ExtraText,
+  TextLink,
+  TextLinkContent,
+} from "../../components/styles";
+
+const { brand, darkLight } = Colors;
+
+export default function SubmitCaseDrawer() {
+  const [hidePassword, setHidePassword] = useState(true);
+  const navigation = useNavigation();
+
+
+  return (
+    <KeyboardAvoidingWrapper>
+      <StyledContainer>
+        <StatusBar style="dark" />
+        <InnerContainer>
+          <PageTitle>Case Details</PageTitle>
+
+          <Formik
+            initialValues={{ email: "", password: "" }}
+            onSubmit={(values, { setSubmitting }) => {
+              handleLogin(values, setSubmitting);
+            }}
+          >
+            {({ handleChange, handleBlur, handleSubmit, values }) => {
+              return (
+                <StyledFormArea style={{ width: "90%" }}>
+                  <BreakPaddingTop />
+                  <LoginTextInput
+                    label="Email Address"
+                    icon="mail"
+                    placeholder="nyayaforyou@gmail.com"
+                    placeholderTextColor={darkLight}
+                    onChangeText={handleChange("email")}
+                    onBlur={handleBlur("email")}
+                    value={values.email}
+                    keyboardType="email-address"
+                  />
+
+                  <BreakPaddingTop />
+                  <BreakPaddingTop />
+                  <LoginTextInput
+                    label="Password"
+                    icon="lock"
+                    placeholder="* * * * * * * * * *"
+                    placeholderTextColor={darkLight}
+                    onChangeText={handleChange("password")}
+                    onBlur={handleBlur("password")}
+                    value={values.password}
+                    secureTextEntry={hidePassword}
+                    isPassword={true}
+                    hidePassword={hidePassword}
+                    setHidePassword={setHidePassword}
+                  />
+
+                  <BreakPaddingTop />
+                  <MsgBox>. . .</MsgBox>
+                  <BreakPaddingTop />
+
+                  <StyledButton onPress={handleSubmit}>
+                    <ButtonText>Login</ButtonText>
+                  </StyledButton>
+
+                  <BreakPaddingTop doubleGap={true} />
+                  <BreakPaddingTop doubleGap={true} />
+                  <Line />
+                  <ExtraView>
+                    <ExtraText>Don't have an account yet? </ExtraText>
+                    <TextLink>
+                      <TextLinkContent
+                        onPress={() => navigation.navigate("Signup")}
+                      >
+                        Sign Up
+                      </TextLinkContent>
+                    </TextLink>
+                  </ExtraView>
+                </StyledFormArea>
+              );
+            }}
+          </Formik>
+        </InnerContainer>
+      </StyledContainer>
+    </KeyboardAvoidingWrapper>
+  );
+};
+
+const LoginTextInput = ({
+  label,
+  icon,
+  isPassword,
+  hidePassword,
+  setHidePassword,
+  ...props
+}) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <Ionicons
+            name={hidePassword ? "md-eye-off" : "md-eye"}
+            size={30}
+            color={darkLight}
+          />
+        </RightIcon>
+      )}
+    </View>
+  );
+};
