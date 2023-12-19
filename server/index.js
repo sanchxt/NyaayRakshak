ac8737bb780d030fb8ebab9c250326689babf087
sanchxt <92094343+sanchxt@users.noreply.github.com>
2023-12-19 15:37:37 +0530

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,15 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 import { PORT, mongoDBURL } from "./config.js";
 import authRoutes from "./routes/authRoutes.js";
+import User from "./models/userModel.js";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.get("/", (request, response) => {
-  console.log(request);
-  return response.status(234).send("Welcome");
+  return response.send("Welcome");
+});
+
+app.get('/getusers', async (request, response) => {
+  try {
+    const users = await User.find({});
+
+    return response.status(200).json(users);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 app.use(authRoutes);
